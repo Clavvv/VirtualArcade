06213d3881c434fb02089a9b7c811fc24446b743
Clavvv <Schpeen@proton.me>
2024-05-24 18:48:15 -0700

    second page

---
+++ app/arcade/page.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+
+export default function Arcade() {
+
+
+
+
+    return (
+
+        <main>
+            This is an empty page
+        </main>
+    )
+
+}
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className= "text-pink-500 text-4xl font-bold">
+      <div className= "text-pink-500[80] text-4xl font-bold">
         Welcome to the arcade!
       </div>
     </main>
