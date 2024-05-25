81ca526fee7cb4f5736041e9b10eea37744b0206
Clavvv <Schpeen@proton.me>
2024-05-25 15:15:17 -0700

    something

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-import Image from "next/image";
+'use client'
+import { useState } from 'react'
+
+
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className= "text-pink-500[80] text-4xl font-bold">
-        Welcome to the arcade!
+      <div className="text-pink-300/80 text-4xl font-bold">
+        This is the home page
       </div>
+
     </main>
   );
 }
