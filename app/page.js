848686bb563dbd34c746611192984f5cb2121bf5
zwcai0110 <zwcai0110@gmail.com>
2024-05-26 01:23:40 -0700

    Added TV

--- app/page.js
+++ app/page.js
@@ -3,16 +3,35 @@ import React, { useState, useRef } from 'react';
 import { EmulatorJS } from 'react-emulatorjs';
 
 export default function Home() {
-  const emulatorArray = ["Game Boy", "Game Boy Advance", "Nintendo DS"];
+  const emulatorArray = ["Gameboy", "Gameboy Adv", "Nintendo DS"];
   const emulatorMap = {
-    "Game Boy": "gb",
-    "Game Boy Advance": "gba",
-    "Nintendo DS": "nds"
+    "Gameboy": "gb",
+    "Gameboy Adv": "gba",
+    "Nintendo DS": "nds",
   };
 
   const gameRef = useRef(null);
   const [rom, setRom] = useState("");
   const [activeEmulator, setActiveEmulator] = useState(emulatorArray[0]);
+  const [fileError, setFileError] = useState(null)
+
+  const verifyRomFileType = (file) => {
+
+    if (file) {
+
+      const extension = file.name.split('.').pop()
+
+      if (extension !== emulatorMap[activeEmulator]) {
+        setFileError("invalid file type")
+        return false
+      }
+
+      setFileError(null)
+      return true
+    }
+
+
+  }
 
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
@@ -43,8 +62,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-neon">
       <div className="flex flex-row items-center">
-        <button 
-          className="cyber-button mx-5" 
+        <button
+          className="cyber-button mx-5"
           onClick={handleEmulatorChangeLeft}
         >
           &lt;
@@ -52,23 +71,32 @@ export default function Home() {
         <span className="text-center text-neon-pink text-4xl font-bold min-w-[250px]">
           {activeEmulator}
         </span>
-        <button 
-          className="cyber-button mx-5" 
-          onClick={handleEmulatorChangeRight}
-        >
-          &gt;
+        <button className="cyber-button mx-5" onClick={handleEmulatorChangeRight}>
+          {">"}
         </button>
+      </div >
+
+      <div className="relative flex justify-center items-center w-full h-full mb-10">
+        <div className="relative w-full xl:max-w-4xl">
+          <img src="/images/tv.png" className="block w-full h-auto" />
+          <div className="absolute inset-0 flex justify-center items-center" style={{ left: '-18%' }}>
+            {rom && (
+              <EmulatorJS
+                className="w-[70%] h-[70%]"
+                EJS_core={emulatorMap[activeEmulator]}
+                EJS_gameUrl={rom}
+              />
+            )}
+          </div>
+          {fileError && <span className="text-sm text-red-600 font-light">{fileError}</span>}
+        </div>
       </div>
-      <div className="flex flex-col h-full w-full">
-        <div className="flex justify-center">
-          {rom && <EmulatorJS className="justify-center" EJS_core={emulatorMap[activeEmulator]} EJS_gameUrl={rom} />}
-        </div>
-        <div className="flex flex-row justify-center mt-5 p-3">
-          <input className="hidden" type="file" ref={gameRef} onChange={handleFileUpload} />
-          <button className="cyber-button" onClick={handleButtonClick}>
-            Load Rom
-          </button>
-        </div>
+
+      <div className="flex flex-row justify-center mt-5 p-3">
+        <input className="hidden" type="file" ref={gameRef} onChange={handleFileUpload} />
+        <button className="cyber-button" onClick={handleButtonClick}>
+          Load Rom
+        </button>
       </div>
     </main>
   );
